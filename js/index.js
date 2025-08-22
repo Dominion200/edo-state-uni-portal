cdfa071bcc42e595108f4383d1440b0ba55b0296
Dominion Jerry <chukwuekudominionjerry@gmail.com>
2025-08-22 09:45:39 -0700

    fix broken ancor link

--- js/index.js
+++ js/index.js
@@ -1,6 +1,8 @@
 const burgerMenu = document.getElementById("burger-menu");
 const menu = document.querySelector(".menu-container");
 
+const menuItems = document.querySelectorAll("#menu-list > li > a");
+
 const menuClassList = menu.classList;
 
 burgerMenu.addEventListener("click", () => {
@@ -10,4 +12,13 @@ burgerMenu.addEventListener("click", () => {
   menuClassList.add("hide-menu");
 });
 
-// LINK STATE MANAGEMENT
+menuItems.forEach((link) => {
+  link.addEventListener("click", (e) => {
+    // e.preventDefault();
+    // console.log(menuClassList);
+    if (menuClassList.contains("hide-menu")) {
+      return menuClassList.remove("hide-menu");
+    }
+    // menuClassList.remove("hide-menu");
+  });
+});
